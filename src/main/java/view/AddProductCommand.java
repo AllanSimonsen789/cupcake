4e130580faf28effb3b7c4066f9f88d8f5f9a293
789Michael <allanalli@hotmail.com>
2019-10-29 18:19:56 +0100

    merge

--- src/main/java/view/AddProductCommand.java
+++ src/main/java/view/AddProductCommand.java
@@ -10,7 +10,6 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import model.Cupcake;
 import model.OrderLine;
 
 /**
